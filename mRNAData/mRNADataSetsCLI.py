6847e601e59b63b6969c7e8625dc117ba325ad7d
Sara Gosline <sara.gosline@pnnl.gov>
2020-11-03 12:25:08 -0800

    fixed proteomic cwl and added mrna cwl
    only for four cancer types, but a start

---
+++ mRNAData/mRNADataSetsCLI.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python
+'''
+Basic CLI to import CPTAC proteomic data
+'''
+
+import cptac
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cancerType', dest='type',\
+                        help='Cancer type to be collected')
+    opts = parser.parse_args()
+
+    if opts.type.lower() == 'brca':
+        dat = cptac.Brca()
+    elif opts.type.lower() == 'ccrcc':
+        dat = cptac.Ccrcc()
+    elif opts.type.lower() == 'coad':
+        dat = cptac.Colon()
+    elif opts.type.lower() == 'ovca':
+        dat = cptac.Ovarian()
+    else:
+        exit()
+
+    df = dat.get_transcriptomics()
+    df.to_csv(df, sep="\t", path="file.tsv")
+
+if __name__=='__main__':
+    main()
