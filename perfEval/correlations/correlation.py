3e9c101d780cb240a13e447109b6a07c979e8837
Song <fengzhimu@outlook.com>
2020-12-07 16:26:55 -0800

    updated all the required implementations and tested the workflow

--- perfEval/correlations/correlation.py
+++ perfEval/correlations/correlation.py
@@ -10,22 +10,27 @@ def main():
                         help='Deconvoluted matrix of transcriptomics data')
     parser.add_argument('--proteomics', dest='profile',
                         help='Deconvoluted matrix of proteomics data')
-    parser.add_argument('--output', dest='output',
-                        help='Output file for the correlation values')
+    # parser.add_argument('--output', dest='output',
+    #                     help='Output file for the correlation values')
     opts = parser.parse_args()
 
-    rna = pd.read_csv(opts.rnafile, sep='\t')
-    pro = pd.read_csv(opts.profile, sep='\t')
+    rna = pd.read_csv(opts.rnafile, sep='\t', index_col=0)
+    pro = pd.read_csv(opts.profile, sep='\t', index_col=0)
 
     rnaCols = list(rna.columns)
     proCols = list(pro.columns)
-    if len(rnaCols) != len(proCols) or len(set(rnaCols + proCols)) != len(proCols):
-        raise ValueError(
-            "The colums of proteomics matrix and transcriptomics matrix are not the same")
-    correlations = pd.Series([pro[sample].corr(rna[sample])
-                              for sample in proCols])
+    intersected = list(set(rnaCols) & set(proCols))
+    if len(rnaCols) != len(proCols) or len(intersected) != len(proCols):
+        print(
+            "The colums of proteomics matrix and transcriptomics matrix are not the same!\n")
+    rna = rna[intersected]
+    pro = pro[intersected]
+    rnaCols = list(rna.columns)
+    proCols = list(pro.columns)
+    corrList = [pro[sample].corr(rna[sample]) for sample in proCols]
+    correlations = pd.Series(corrList)
     correlations.index = proCols
-    correlations.to_csv(opts.output, sep='\t')
+    correlations.to_csv("corr.tsv", sep='\t', header=False)
 
 
 if __name__ == '__main__':
