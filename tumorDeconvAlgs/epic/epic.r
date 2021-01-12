90f8a1ce568ac1387d68ecab7abfbdc23cc156f2
Song <fengzhimu@outlook.com>
2021-01-12 01:46:54 -0800

    update

--- tumorDeconvAlgs/epic/epic.r
+++ tumorDeconvAlgs/epic/epic.r
@@ -1,14 +1,31 @@
 #!/usr/local/bin/env Rscript --vanilla
 args <- commandArgs(TRUE)
-file0 <- args[1]
-ref <- NULL
-if (!is.null(args[2])) {
-    file1 <- args[1]
-    ref <- read.csv(file1, sep = "\t", row.names = 1) ### Need to change later
+if (!is.null(args[1])) {
+    df <- read.csv(args[1], sep = "\t", row.names = 1)
+} else {
+    stop("No expression matrix provided!")
 }
 
 library(EPIC)
 
-df <- read.csv(file0, sep = "\t", row.names = 1)
-xc <- EPIC(bulk = df, reference = ref)
-write.table(xc, file = "deconvoluted.tsv", quote = FALSE, col.names = NA, sep = "\t")
+if (!is.null(args[2])) {
+    ref <- read.csv(args[2], sep = "\t", row.names = 1) ### Need to change later
+    markerGenes <- c()
+    for (s in colnames(ref)) {
+        genesTemp <- rownames(ref[ref[,s] > quantile(ref[,s], prob=0.90),])
+        markerGenes <- c(markerGenes, genesTemp)
+        genesNull <- rownames(ref[ref[,s] <= quantile(ref[,s], prob=0.90),])
+        ref[genesNull, s] <- 0.0
+    }
+    markerGenes <- unique(markerGenes)
+    
+    epicRef <- list()
+    epicRef$refProfiles <- ref
+    epicRef$sigGenes <- markerGenes
+    
+    xc <- EPIC(bulk = df, reference = epicRef)
+} else {
+    xc <- EPIC(bulk = df)
+}
+
+write.table(xc$cellFractions, file = "deconvoluted.tsv", quote = FALSE, col.names = NA, sep = "\t")
