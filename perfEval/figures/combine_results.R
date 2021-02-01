87e56f2837b114f5095e280ae54540c8ce2d0b72
Sara Gosline <sara.gosline@pnnl.gov>
2021-02-01 09:43:48 -0800

    added in mrna algorithm
    not ideal plotting, but good enough

--- perfEval/figures/combine_results.R
+++ perfEval/figures/combine_results.R
@@ -1,9 +1,9 @@
 #' merge results across matrices, algorithms and cancers
-#' currently we have two assessments: 
+#' currently we have two assessments:
 #' 1 - assess correlations for each patient/disease
 #' 2- assess correlations for each celltype/matrix
-#' 
-#' 
+#'
+#'
 #' to run:
 #' Rscript  combine_results.R --sample */*corr.tsv
 #' Rscript  combine_results.R --cellType  */*corrXcelltypes.tsv
@@ -18,20 +18,23 @@ combinePatientCors<-function(file.list){
   full.tab<-do.call(rbind,lapply(file.list,function(file){
       vars <- unlist(strsplit(basename(file),split='-')) #split into pieces
       disease=vars[1]
-      algorithm=vars[2]
-      matrix=vars[3]
+      mrna.algorithm=vars[2]
+      prot.algorithm=vars[3]
+      matrix=vars[4]
       tab<-read.table(file,fill=TRUE)
       colnames(tab)<-(c('patient','correlation'))
-      return(data.frame(tab,disease,algorithm,matrix))
+      return(data.frame(tab,disease,mrna.algorithm,prot.algorithm.matrix))
   }))
-  
+
+   full.tab<-full.tab%>%mutate(algorithm=paste(mrna.algorithm,prot.algorithm))
+
   p <- ggplot(full.tab)+
     geom_violin(aes(x=algorithm,y=correlation,fill=disease))+
     facet_grid(matrix~.)+scale_fill_viridis_d()+
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   ggsave('patientCors.pdf',p,width=11,height=10)
   return(full.tab)
-   
+
 }
 
 #' combine list of files by cell type correlations
@@ -46,22 +49,22 @@ combineCellTypeCors<-function(file.list){
       colnames(tab)<-(c('cellType','correlation'))
       return(data.frame(tab,disease,algorithm,matrix))
   }))
-   
+
    require(cowplot)
-   
+
    plist<-lapply(unique(full.tab$matrix),function(m){
      stab<-subset(full.tab,matrix==m)
      stab$cellType<-factor(stab$cellType)
      ggplot(stab)+geom_jitter(aes(x=cellType,y=correlation,color=algorithm,shape=disease))+
-       scale_color_viridis_d()+    
+       scale_color_viridis_d()+
        theme(text = element_text(size=20),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
                ggtitle(m)
    })
-   
+
    p<-cowplot::plot_grid(plotlist=plist)
 
    ggsave('cellTypeCors.pdf',p,width=20,height=15)
-   
+
   return(full.tab)
 }
 
@@ -84,14 +87,14 @@ main<-function(){
     write.table(tab,'combinedCellTypeCorrelationTabl.csv',row.names=F,col.names=T)
   }else{
     print("First argument must be `cellType` or `sample`")
-    
-  }
-  
 
-  
+  }
+
+
+
   ##plot
-  
-  
+
+
 
 }
 
