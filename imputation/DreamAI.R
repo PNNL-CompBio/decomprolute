1c4035d449d34b2b738d03bdd16a5cb0aa859a5c
Anna Calinawan <anna.pamela@gmail.com>
2021-02-05 16:52:40 -0500

    Add line to remove rows where all data is NA (found in endometrial)

--- imputation/DreamAI.R
+++ imputation/DreamAI.R
@@ -10,14 +10,17 @@ require("iterators")
 require("Matrix")
 require("impute")
 require("DreamAI")
+library("tidyr")
 
 args <- commandArgs(trailingOnly = TRUE)
 argsLen <- length(args)
+use_MissForest <- FALSE
 input_f <- args[1]
 use_MissForest <- if (argsLen < 2) TRUE else args[2];
 
 data = read.csv(input_f, header=TRUE, row.names=1, sep = '\t')
-
+# remove rows where all data is NA
+data = data[rowSums(is.na(data)) != ncol(data), ]
 
 if (use_MissForest) {
     impute<- DreamAI(data,k=10,maxiter_MF = 10, ntree = 100,maxnodes = NULL,maxiter_ADMIN=30,tol=10^(-2),gamma_ADMIN=NA,gamma=50,CV=FALSE,fillmethod="row_mean",maxiter_RegImpute=10,conv_nrmse = 1e-6,iter_SpectroFM=40, method = c("KNN", "MissForest", "ADMIN", "Birnn", "SpectroFM", "RegImpute"),out="Ensemble")
