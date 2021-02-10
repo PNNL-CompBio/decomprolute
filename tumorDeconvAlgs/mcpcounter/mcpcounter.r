488aa7e1ac3b0339a0f6169a5584ab3bccf107e0
Song <fengzhimu@outlook.com>
2021-02-09 17:51:50 -0800

    updated the processing of signature matrices to make consistent marker gene list extraction

--- tumorDeconvAlgs/mcpcounter/mcpcounter.r
+++ tumorDeconvAlgs/mcpcounter/mcpcounter.r
@@ -2,18 +2,33 @@
 args <- commandArgs(TRUE)
 if (!is.null(args[1])) {
     df <- read.csv(args[1], sep = "\t", row.names = 1)
+    if (max(df, na.rm = TRUE) < 50) {
+        df <- 2^df
+    }
 } else {
     stop("No expression matrix provided!")
 }
 
 library(MCPcounter)
 
-if (!is.null(args[2])) {
+if (length(args) > 1) {
     ref <- read.csv(args[2], sep = "\t", row.names = 1) ### Need to change later
+    cellTypeNames <- colnames(ref)
     markerGenes <- c()
     cellTypes <- c()
     for (s in colnames(ref)) {
-        genesTemp <- rownames(ref[ref[,s] > quantile(ref[,s], prob=0.90),])
+        tempMarkers <- rownames(ref[ref[,s] > quantile(ref[,s], prob=0.75),])
+        if (length(args) > 2) {
+            if (tolower(args[3]) == "pairwise") {
+                tempTb = 2 * ref[,cellTypeNames[cellTypeNames != s]] - ref[,s]
+                tempMarkers <- tempMarkers[tempMarkers %in% rownames(ref[rowSums(tempTb < 0) == (length(cellTypeNames) - 1), ])]
+            } else {
+                stop("The signature matrix processing option is not provided correctly!")
+            }
+        } else {
+            tempMarkers <- tempMarkers[tempMarkers %in% rownames(ref)[ref[,s] > 2 * rowMeans(ref)]]
+        }
+        genesTemp <- tempMarkers
         markerGenes <- c(markerGenes, genesTemp)
         cellTypes <- c(cellTypes, rep(s, length(genesTemp)))
     }
