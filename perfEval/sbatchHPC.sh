646dc125e3378c4388b5da74e3d68df32b9fd504
Song Feng <feng626@constance7a.local>
2021-02-25 18:34:55 -0800

    change the singularity cache directory

--- perfEval/sbatchHPC.sh
+++ perfEval/sbatchHPC.sh
@@ -11,14 +11,18 @@
 
 
 module purge
-module load singularity/3.6.3
 module load python/anaconda3.2019.3
 source /share/apps/python/anaconda3.2019.3/etc/profile.d/conda.sh
 conda init zsh
 conda activate omics
 
 export PATH=/people/feng626/.conda/envs/omics/bin:$PATH
+
 export PATH=/people/feng626/.nvm/versions/node/v15.8.0/bin:$PATH
+
+module load singularity/3.6.3
+
+export SINGULARITY_CACHEDIR=/pic/scratch/feng626/singularity
 
 cd /pic/scratch/feng626/proteomicsTumorDeconv/perfEval
 
