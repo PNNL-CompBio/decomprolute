15b3ef85ed69f44be246deed07a9bce8854c1502
Song Feng <feng626@constance01.pnl.gov>
2021-02-26 10:06:45 -0800

    update

--- perfEval/sbatchHPC.sh
+++ perfEval/sbatchHPC.sh
@@ -22,20 +22,20 @@ export PATH=/people/feng626/.nvm/versions/node/v15.8.0/bin:$PATH
 
 module load singularity/3.6.3
 
-export SINGULARITY_HOME=/pic/scratch/feng626/singularity
-export SINGULARITYENV_HOME=/pic/scratch/feng626/singularity
-export SINGULARITY_CACHEDIR=/pic/scratch/feng626/singularity/singularity_cache
-export SINGULARITYENV_CACHEDIR=/pic/scratch/feng626/singularity/singularity_cache
-export SINGULARITY_TMPDIR=/pic/scratch/feng626/singularity/singularity_tmp
-export SINGULARITYENV_TMPDIR=/pic/scratch/feng626/singularity/singularity_tmp
-export CWL_SINGULARITY_CACHE=/pic/scratch/feng626/singularity/singularity_cwl
+export SINGULARITY_HOME=/people/feng626/singularity
+export SINGULARITYENV_HOME=/people/feng626/singularity
+export SINGULARITY_CACHEDIR=/people/feng626/.singularity
+export SINGULARITYENV_CACHEDIR=/people/feng626/.singularity
+#export SINGULARITY_TMPDIR=/people/feng626/.singularity/tmp
+#export SINGULARITYENV_TMPDIR=/people/feng626/.singularity/tmp
+export CWL_SINGULARITY_CACHE=/people/feng626/.singularity/cwl
 
 export XDG_RUNTIME_DIR=/people/feng626/temp
 
 cd /pic/scratch/feng626/proteomicsTumorDeconv/perfEval
 
-cwltool --singularity scatter-imputation.cwl fig4-eval.yml
-# cwltool --singularity --parallel scatter-imputation.cwl fig4-eval.yml
+#cwltool --singularity scatter-imputation.cwl fig4-eval.yml
+cwltool --singularity --parallel scatter-imputation.cwl fig4-eval.yml
 
 
 
