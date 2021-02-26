bf5bccc15494f16572ddb25835df99d78b162912
Song Feng <feng626@constance7a.local>
2021-02-25 20:04:42 -0800

    change the singularity cache directory

--- perfEval/sbatchHPC.sh
+++ perfEval/sbatchHPC.sh
@@ -22,12 +22,18 @@ export PATH=/people/feng626/.nvm/versions/node/v15.8.0/bin:$PATH
 
 module load singularity/3.6.3
 
-export SINGULARITY_CACHEDIR=/pic/scratch/feng626/singularity
-export CWL_SINGULARITY_CACHE=/pic/scratch/feng626/singularity
+export SINGULARITY_HOME=/pic/scratch/feng626/singularity
+export SINGULARITYENV_HOME=/pic/scratch/feng626/singularity
+export SINGULARITY_CACHEDIR=/pic/scratch/feng626/singularity/singularity_cache
+export SINGULARITYENV_CACHEDIR=/pic/scratch/feng626/singularity/singularity_cache
+export SINGULARITY_TMPDIR=/pic/scratch/feng626/singularity/singularity_tmp
+export SINGULARITYENV_TMPDIR=/pic/scratch/feng626/singularity/singularity_tmp
+export CWL_SINGULARITY_CACHE=/pic/scratch/feng626/singularity/singularity_cwl
 
 cd /pic/scratch/feng626/proteomicsTumorDeconv/perfEval
 
 cwltool --singularity scatter-imputation.cwl fig4-eval.yml
+# cwltool --singularity --parallel scatter-imputation.cwl fig4-eval.yml
 
 
 
