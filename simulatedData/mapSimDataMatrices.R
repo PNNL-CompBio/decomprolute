7a8879b2ae09995efdefe9522616a8dd2f86d58c
Sara Gosline <sara.gosline@pnnl.gov>
2021-04-21 14:00:16 -0700

    updated data mapping

--- simulatedData/mapSimDataMatrices.R
+++ simulatedData/mapSimDataMatrices.R
@@ -3,8 +3,18 @@ main <- function(){
     deconv.mat <- read.table(argv[1],header=T,row.names=1,sep='\t') #matrix to be fixed
     sig.mat <- argv[2] #signature used to deconvolve matrix
     sim.type <- argv[3] #type of simulation performed
-
-    if(sig.mat=='LM9.txt')
+    sig.mat<-basename(sig.mat)
+    
+    if(sig.mat=='LM9.txt'){
+        
+    }else if(sig.mat=='LM22.txt'){
+        
+    }else if(sig.mat=='LM7c.txt'){
+        
+    }else if(sig.mat=='PBMC'){
+        
+    }
+    
     write.table(deconv.mat,file='fixed.tsv',sep='\t')
 }
 
