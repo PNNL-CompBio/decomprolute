7c41143f52da5cc708bbfb475ee4cebbbbdf138c
Sara Gosline <sara.gosline@pnnl.gov>
2021-05-14 14:17:36 -0700

    updated to remove manubot

--- manuscript/build/autobuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-## autobuild.sh: automatically rebuild mansucript outputs and the webpage when content changes
-## Depends on watchdog https://github.com/gorakhargosh/watchdog
-
-watchmedo shell-command \
-  --wait \
-  --command='bash build/build.sh && manubot webpage' \
-  content
