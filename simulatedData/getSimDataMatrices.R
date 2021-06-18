ab1836c918153e85abcf1e4e4a47980dafaf5d6a
Sara Gosline <sara.gosline@pnnl.gov>
2021-06-18 12:12:04 -0700

    updated figures further

--- simulatedData/getSimDataMatrices.R
+++ simulatedData/getSimDataMatrices.R
@@ -20,25 +20,25 @@ main<-function(){
 
   simdata = argv[1]
   print(simdata)
-  if(simdata=='pbmc'){
-    dataToTxt(paste0('/bin/pbmc_data_mix.rda'))
-  }else{
-    datarep=argv[2]
-    if(simdata=='prot'){
-      if(!datarep%in%c('1','2','3','4','5')){
-        print(paste0('No proteomics data file for',datarep))
-      }else{
-        dataToTxt(paste0('/bin/data_rep_',datarep,'.rda'))
+  #if(simdata=='pbmc'){
+                                        #  dataToTxt(paste0('/bin/pbmc_data_mix.rda'))
+  #}else{
+  datarep=argv[2]
+  if(simdata=='prot'){
+     if(!datarep%in%c('1','2','3','4','5')){
+       print(paste0('No proteomics data file for',datarep))
+     }else{
+       dataToTxt(paste0('/bin/data_rep_',datarep,'.rda'))
       }
   }else if(simdata=='mrna'){
-      if(!datarep%in%c('1','2','3','4','5','6','7','8','9','10')){
+      if(!datarep%in%c('1','2','3','4','5','6','7','8','9','10','pbmc')){
         print(paste0('No rna data file for',datarep))
-      }else{
+  }else if(datarep=='pbmc'){
+       dataToTxt(paste0('/bin/pbmc_data_mix.rda'))
+  }else{
         dataToTxt(paste0('/bin/gse60424_',datarep,'.rda'))
       }
-    }
-  }
-
+ }
 }
 
 main()
