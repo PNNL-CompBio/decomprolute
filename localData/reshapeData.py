928c662303c3cabdc5fd5ba4f3915f8d9ad67b8b
Sara Gosline <sara.gosline@pnnl.gov>
2022-05-30 20:50:30 -0700

    added some more test scripts

---
+++ localData/reshapeData.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+allfiles = [a for a in os.listdir('data') if 'tsv' in a]
+for fname in allfiles:
+    fname = 'data/'+fname
+    print(fname)
+    tab = pd.read_csv(fname,sep='\t')
+    if "HGNC_Approved_Symbol" in tab.columns:
+        tab = tab.iloc[:,2:].groupby('HGNC_Approved_Symbol').sum()
+    elif 'gene_name' in tab.columns:
+        tab = tab.iloc[:,1:].groupby('gene_name').sum()
+    else:
+        print(tab.columns)
+    tab.to_csv('new'+fname,sep='\t')
