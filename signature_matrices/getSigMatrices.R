a2bebaf8f5e34a67f36375672408373c1291a963
Sara JC Gosline <sara.gosline@pnnl.gov>
2022-11-10 13:52:51 -0800

    Update getSigMatrices.R

--- signature_matrices/getSigMatrices.R
+++ signature_matrices/getSigMatrices.R
@@ -12,9 +12,13 @@ main<-function(){
                            gsub('...6','',colnames(tab)))
       tab<-tab[1:50,]
       tab$LSC17=c('DNMT3B','ZBTB46','NYNRIN','ARHGAP22','LAPTM4B','MMRN1','DPYSL3','KIAA0125','CDK6','CPXM1','GPR56','AKR1C3','CD34','NGFRAP1','EMP1','SMIM24','SOCS2',rep('',33))
-      tab<-tab%>%pivot_longer(cols=c(1:ncol(tab)),names_to='cellType',values_to='gene')%>%mutate(value=1)%>%pivot_wider(names_from=cellType,values_fill=0,values_fn=mean)
+      tab<-tab%>%
+        pivot_longer(cols=c(1:ncol(tab)),names_to='cellType',values_to='gene')%>%
+        mutate(value=1)%>%
+        pivot_wider(names_from=cellType,values_fill=0,values_fn=mean)%>%
+        tibble::column_to_rownames("gene")
       
-      write.table(tab,'./AML.txt',row.names=T,sep='\t')
+      write.table(tab,'./AML.txt',row.names=T,sep='\t',quote=F)
 
     }
     else
