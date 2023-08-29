181b6a80f494193cb91d913cf1e4ebcf2d483f96
Sara JC Gosline <sara.gosline@pnnl.gov>
2023-08-29 11:22:27 -0700

    forgot the new matrix function

--- signature_matrices/getSigMatrices.R
+++ signature_matrices/getSigMatrices.R
@@ -7,8 +7,8 @@ main<-function(){
     argv <- commandArgs(trailingOnly = TRUE)
     sig_name <- trimws(argv[1])
 
-    sample <-as.numeric(trimws(argv[2]))
-    print(sample)    
+    sampval <-as.numeric(trimws(argv[2]))
+    print(samval)    
     #reshape matrisome to adhere to standards
     if(tolower(sig_name)=='matrisome'){
       tab <- readxl::read_xlsx('/Hs_Matrisome_Masterlist_Naba et al_2012.xlsx')[,c(2:3)]|>
@@ -25,10 +25,10 @@ main<-function(){
     tab <- read.table(paste0('/',sig_name,'.txt'),header=T,sep='\t',row.names=1,check.names=F)
     num<-nrow(tab)
     prots<-rownames(tab)
-    subsamp<-sample(prots,size=round(length(prots)*sample/100),replace=FALSE)
+    subsamp<-sample(prots,size=round(length(prots)*sampval/100),replace=FALSE)
     print(paste('reducing signature matrix from',nrow(tab),'to',length(subsamp)))
     tab<-tab[subsamp,]
-    sig_name <- paste0(sig_name,'_',sample)
+    sig_name <- paste0(sig_name,'_',sampval)
     write.table(tab,paste0('./',sig_name,'.txt'),row.names=T,sep='\t',quote=F)
 
 }
